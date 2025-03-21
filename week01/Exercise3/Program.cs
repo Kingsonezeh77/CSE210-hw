cde35f1c56e1c5f1604a95a43bab0a25c068bc36
Simeon Ezeh <kingsonsimeon@gmail.com>
2025-03-21 13:40:49 +0100

    Finished week 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string keepPlaying = "yes";
+
+        // As long as they say "yes" we will keep playing
+        while (keepPlaying.ToLower() == "yes")
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            // This variable will keep track of how many guesses it took
+            int guessCount = 0;
+
+            int guess = -1;
+
+            // Keep going as long as the guess doesn't match the magic number
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // At this point, they have guessed it correctly
+            Console.WriteLine($"It took you {guessCount} guesses");
+
+            Console.Write("Would you like to play again (yes/no)? ");
+            keepPlaying = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thank you for playing. Goodbye.");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        Console.WriteLine("Enter numbers to add to the list (type 0 to stop):");
+        var numbersList = new System.Collections.Generic.List<int>();
+        int input;
+
+
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+                if (input == 0)
+                {
+                    break;
+                }
+                numbersList.Add(input);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+
+        if (numbersList.Count > 0)
+        {
+            numbersList.Sort();
+
+            int min = numbersList.First();
+            int max = numbersList.Last();
+
+            Console.WriteLine($"You entered the following numbers: {string.Join(", ", numbersList)}");
+            Console.WriteLine($"The minimum value is: {min}");
+            Console.WriteLine($"The maximum value is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
+
+
